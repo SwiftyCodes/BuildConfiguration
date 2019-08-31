fb8e6ea2d8ece73635d61523caf4f41d0191da26
Anurag <swiftycodes@gmail.com>
2019-08-31 22:18:56 +0530

    Initial Commit

---
+++ BuildConfiguration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuildConfiguration
+//
+//  Created by Anurag Kashyap on 31/08/19.
+//  Copyright © 2019 Anurag Kashyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
